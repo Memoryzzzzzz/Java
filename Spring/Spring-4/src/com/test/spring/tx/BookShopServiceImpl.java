5228a3c2a644ee5cb1f509ddd0f1fbaa8936593c
Memory <zyh1664534928@163.com>
2019-04-02 21:45:13 +0800

    20170402

--- Spring/Spring-4/src/com/test/spring/tx/BookShopServiceImpl.java
+++ Spring/Spring-4/src/com/test/spring/tx/BookShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.test.spring.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +13,26 @@ public class BookShopServiceImpl implements BookShopService {
     private BookShopDao bookShopDao;
 
     // 添加事务注解
-    // 使用 propagation 指定事务的传播行为，即当前的事务方法被另一个事务方法调用时
+    // 1. 使用 propagation 指定事务的传播行为，即当前的事务方法被另一个事务方法调用时
     // 如何使用事务，默认取值为 REQUIRED，即使用调用方法的事务
     // REQUIRES_NEW：事务自己的事务，调用的事务方法的事务被挂起。
-    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    // 2. 使用 isolation 指定事务的隔离级别，最常用的取值为 READ_COMMITTED
+    // 3. 默认情况下 Spring 的声明式事务对所有的运行时异常进行回滚，
+    // 也可以通过对应的属性进行设置，通常情况下取默认值即可
+    // 4. 使用 readOnly 指定事务是否为只读，表示这个事务只读取数据但不更新数据，
+    // 这样可以帮助数据库引擎优化事务，若是一个只读取数据库值的方法，应设置 readOnly = true
+    // 5. 使用 timeout 指定强制回滚之前事务可以占用的时间。
+    @Transactional(propagation = Propagation.NOT_SUPPORTED,
+            isolation = Isolation.READ_COMMITTED,
+            noRollbackFor = {UserAccountException.class},
+            readOnly = false,
+            timeout = 3)
     @Override
     public void purchase(String username, String isbn) {
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {}
 
         // 1. 获取书的单价
         int price = bookShopDao.findBookPriceByIsbn(isbn);
